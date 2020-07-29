6a5125d73d68a26470e912e980fa89eebb4a57c6
fakeboyas <ovoivi9@gmail.com>
2020-07-29 19:38:44 +0700

    finish task

--- src/App.js
+++ src/App.js
@@ -1,12 +1,12 @@
 import React from 'react';
 import logo from './logo.svg';
 import './App.css';
-import RegisterSpeaker from './pages/RegisterSpeaker'
+
 
 function App() {
   return (
     <div className="App">
-     <RegisterSpeaker/>
+   
     </div>
   );
 }
