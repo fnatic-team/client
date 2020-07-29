9ab9e6b657320881b0cdc741f7e38d4d04c7e9bb
fakeboyas <ovoivi9@gmail.com>
2020-07-29 15:13:42 +0700

    login Page before social login

--- src/pages/Login/Login.js
+++ src/pages/Login/Login.js
@@ -5,8 +5,8 @@ import styled from "styled-components";
 
 const LoginWrapper = styled.div`
   width: 100%;
-  max-width: 400px;
-  padding: 15px;
+  max-width: 500px;
+  padding: 60px;
   margin: auto;
   height: 100%;
   border: 1px black solid;
@@ -16,7 +16,7 @@ function Login() {
   return (
     <div>
       <LoginWrapper>
-        <h2 className="text-center">Login</h2>
+        <h2 className="text-center">Sign In</h2>
         <FormGroup>
           <Label>Email</Label>
           <Input
@@ -37,14 +37,15 @@ function Login() {
             placeholder="Your password.."
           />
         </FormGroup>
-        <button className="btn btn-lg btn-primary btn-block">Login</button>
+        <button className="btn btn-primary btn-block">Login</button>
         <div className="text-center pt-3 ">
-          <span>Or</span>
+          <p>Or</p>
         </div>
-        <button className="btn btn-lg btn-block" style={{backgroundColor:"#3b5998", color:"white"}}>Facebook</button>
-        <button className="btn btn-lg btn-success btn-block">Google</button>
+        <button className="btn  btn-block" style={{backgroundColor:"#3b5998", color:"white"}}>Facebook</button>
+        <button className="btn  btn-success btn-block">Google</button>
         {/* <Google /> */}
         <div className="text-center">
+            <br/>
          <span>Belum Punya Akun? Sign Up</span>
         </div>
       </LoginWrapper>
