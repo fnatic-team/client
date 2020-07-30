8de461ad86e2ff6ba8c07f1ab763b366aa1cad03
fakeboyas <ovoivi9@gmail.com>
2020-07-30 08:58:34 +0700

    finish dummy browser Speaker

--- src/App.js
+++ src/App.js
@@ -1,12 +1,11 @@
 import React from 'react';
 import logo from './logo.svg';
 import './App.css';
-import BrowserSpeaker from './pages/BrowserSpeaker'
 
 function App() {
   return (
     <div className="App">
-     <BrowserSpeaker/>
+
     </div>
   );
 }
