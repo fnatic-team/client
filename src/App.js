e312d2c32d6f13f942d96974747bd48820f9fa3f
fakeboyas <ovoivi9@gmail.com>
2020-07-30 09:00:06 +0700

    clear App

--- src/App.js
+++ src/App.js
@@ -1,13 +1,13 @@
 import React from 'react';
 
 import './App.css';
-import RegisterAudience from './pages/RegisterAudience/RegisterAudience'
+
 
 
 function App() {
   return (
     <div className="App">
-      <RegisterAudience/>
+     
     </div>
   );
 }
