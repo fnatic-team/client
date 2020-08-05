315149c75f88f62c4dc5c9b3ec4693b9349f4c7d
fakeboyas <ovoivi9@gmail.com>
2020-08-05 19:09:15 +0700

    fix update data

--- src/pages/DetailSpeaker/DetailSpeaker.js
+++ src/pages/DetailSpeaker/DetailSpeaker.js
@@ -123,22 +123,7 @@ return <> {dataSpeaker !== null ? (
                                 <div className="text-left d-flex flex-column h-100 justify-content-center">
                                     <h4 className="font-weight-bold">Bio</h4>
                                     <p>
-                                        Lorem Ipsum is simply dummy text of the
-                                        printing and typesetting industry. Lorem
-                                        Ipsum has been the industry's standard
-                                        dummy text ever since the 1500s, when an
-                                        unknown printer took a galley of type
-                                        and scrambled it to make a type specimen
-                                        book. It has survived not only five
-                                        centuries, but also the leap into
-                                        electronic typesetting, remaining
-                                        essentially unchanged. It was
-                                        popularised in the 1960s with the
-                                        release of Letraset sheets containing
-                                        Lorem Ipsum passages, and more recently
-                                        with desktop publishing software like
-                                        Aldus PageMaker including versions of
-                                        Lorem Ipsum.
+                                        {dataSpeaker.bio !== null && dataSpeaker.bio}
                                     </p>
                                 </div>
                             </div>
@@ -185,7 +170,7 @@ return <> {dataSpeaker !== null ? (
         ) : (
             <>
                 {" "}
-                <div className="container-fluid">
+                <div className="container-fluid" style={{width:"3rem", height:"3rem", marginTop:"200px"}}>
                     <div className="spinner-border text-primary" role="status">
                         <span className="sr-only">Loading...</span>
                     </div>{" "}
--- src/pages/Profile/Profile.js
+++ src/pages/Profile/Profile.js
@@ -27,160 +27,160 @@ function Profile() {
         // eslint-disable-next-line
     }, []);
 
-   
-        
-
-    return <> {dataSpeaker !== null ? (
-            <>
-                <div style={{ margin: "100px 0px 80px 0px" }}>
-                    <div
-                        className="container"
-                        animation="fade-down"
-                        duration={1000}
-                    >
-                        <div className="row bg-white m-3 border  pad1 shadow-lg">
-                            <div className="col m-3 modal-body text-center align-self-center">
-                                <div className="row">
-                                    <div className="col-sm">
+    return (
+        <>
+            {" "}
+            {dataSpeaker !== null ? (
+                <>
+                    <div style={{ margin: "100px 0px 80px 0px" }}>
+                        <div
+                            className="container"
+                            animation="fade-down"
+                            duration={1000}
+                        >
+                            <div className="row bg-white m-3 border  pad1 shadow-lg">
+                                <div className="col m-3 modal-body text-center align-self-center">
+                                    <div className="row">
                                         <div className="col-sm">
-                                            <img
-                                                className="rounded-circle"
-                                                src={dataSpeaker.image}
-                                                alt=""
-                                                style={{ width: "200px" }}
-                                            />
+                                            <div className="col-sm">
+                                                <img
+                                                    className="rounded-circle"
+                                                    src={dataSpeaker.image}
+                                                    alt=""
+                                                    style={{ width: "200px" }}
+                                                />
+                                            </div>
                                         </div>
-                                    </div>
 
-                                    <div className="col-sm">
-                                        <div className="text-left d-flex flex-column h-100 justify-content-center">
-                                            <h6 className="font-weight-bold">
-                                                {dataSpeaker.name.toTitleCase()}
-                                            </h6>
-                                            <p>{dataSpeaker.category}</p>
-                                            <div>
-                                                <Link to={`/profile/edit/${dataSpeaker._id}`}>
-                                                    <button
-                                                        style={{
-                                                            padding:
-                                                                "10px 30px 10px 30px",
-                                                        }}
-                                                        className="btn rounded-pill btn-xs btn-primary"
+                                        <div className="col-sm">
+                                            <div className="text-left d-flex flex-column h-100 justify-content-center">
+                                                <h6 className="font-weight-bold">
+                                                    {dataSpeaker.name.toTitleCase()}
+                                                </h6>
+                                                <p>{dataSpeaker.category}</p>
+                                                <div>
+                                                    <Link
+                                                        to={`/profile/edit/${dataSpeaker._id}`}
                                                     >
-                                                        Edit Profile
-                                                    </button>
-                                                </Link>
+                                                        <button
+                                                            style={{
+                                                                padding:
+                                                                    "10px 30px 10px 30px",
+                                                            }}
+                                                            className="btn rounded-pill btn-xs btn-primary"
+                                                        >
+                                                            Edit Profile
+                                                        </button>
+                                                    </Link>
+                                                </div>
+                                            </div>
+                                        </div>
+                                        <div
+                                            className="divider"
+                                            style={{
+                                                border: "1px solid",
+                                                width: "1px",
+                                            }}
+                                        ></div>
+
+                                        <div className="col-sm p-4">
+                                            <div className="text-left d-flex flex-column h-100 justify-content-center">
+                                                <div className="d-flex flex-row justify-content-between">
+                                                    <p>City</p>
+                                                    <span>Rp. {dataSpeaker.location !== null && dataSpeaker.location} / jam </span>
+                                                </div>
+                                                <div className="d-flex flex-row justify-content-between">
+                                                    <p>Available to</p>
+                                                    <span>Jakarta</span>
+                                                </div>
+                                                <div className="d-flex flex-row justify-content-between">
+                                                    <p>Fee</p>
+                                                    <span>
+                                                        {dataSpeaker.fee !==
+                                                            null &&
+                                                            dataSpeaker.fee}
+                                                    </span>
+                                                </div>
+                                                <div className="d-flex flex-row justify-content-between">
+                                                    <p>Languages</p>
+                                                    <span>
+                                                        Indonesia , English
+                                                    </span>
+                                                </div>
                                             </div>
                                         </div>
                                     </div>
-                                    <div
-                                        className="divider"
-                                        style={{
-                                            border: "1px solid",
-                                            width: "1px",
-                                        }}
-                                    ></div>
+                                </div>
+                            </div>
+                        </div>
 
-                                    <div className="col-sm p-4">
-                                                <p>City</p>
-                                        <div className="text-left d-flex flex-column h-100 justify-content-center">
-                                            <div className="d-flex flex-row justify-content-between">
-                                                <span>Jakarta</span>
-                                            </div>
-                                            <div className="d-flex flex-row justify-content-between">
-                                                <p>Available to</p>
-                                                <span>Jakarta</span>
-                                            </div>
-                                            <div className="d-flex flex-row justify-content-between">
-                                                <p>Fee</p>
-                                                <span>Rp. 5.000.000 /jam</span>
-                                            </div>
-                                            <div className="d-flex flex-row justify-content-between">
-                                                <p>Languages</p>
-                                                <span>Indonesia , English</span>
-                                            </div>
-                                        </div>
+                        <div className="container">
+                            <div className="row bg-white m-3 p-5 border shadow-lg">
+                                <div className="col-sm">
+                                    <div className="text-left d-flex flex-column h-100 justify-content-center">
+                                        <h4 className="font-weight-bold">
+                                            Bio
+                                        </h4>
+                                        <p>
+                                           {dataSpeaker.bio !== null && dataSpeaker.bio}
+                                        </p>
+                                    </div>
+                                </div>
+                            </div>
+                        </div>
+
+                        <div className="container">
+                            <div className="row bg-white m-3 p-5 border shadow-lg">
+                                <div className="col-sm">
+                                    <h4 className="text-left mb-4 font-weight-bold">
+                                        Social Media
+                                    </h4>
+                                    <div className="row d-flex w-25 justify-content-between ml-2">
+                                        <i
+                                            className="fa fa-2x fa-facebook-square"
+                                            aria-hidden="true"
+                                        ></i>
+
+                                        <i
+                                            className="fa fa-2x fa-linkedin-square"
+                                            aria-hidden="true"
+                                        ></i>
+
+                                        <i
+                                            className="fa fa-2x fa-instagram"
+                                            aria-hidden="true"
+                                        ></i>
+
+                                        <i
+                                            className="fa fa-2x fa-whatsapp"
+                                            aria-hidden="true"
+                                        ></i>
+
+                                        <i
+                                            className="fa fa-2x fa-youtube-play"
+                                            aria-hidden="true"
+                                        ></i>
                                     </div>
                                 </div>
                             </div>
                         </div>
                     </div>
-
-                    <div className="container">
-                        <div className="row bg-white m-3 p-5 border shadow-lg">
-                            <div className="col-sm">
-                                <div className="text-left d-flex flex-column h-100 justify-content-center">
-                                    <h4 className="font-weight-bold">Bio</h4>
-                                    <p>
-                                        Lorem Ipsum is simply dummy text of the
-                                        printing and typesetting industry. Lorem
-                                        Ipsum has been the industry's standard
-                                        dummy text ever since the 1500s, when an
-                                        unknown printer took a galley of type
-                                        and scrambled it to make a type specimen
-                                        book. It has survived not only five
-                                        centuries, but also the leap into
-                                        electronic typesetting, remaining
-                                        essentially unchanged. It was
-                                        popularised in the 1960s with the
-                                        release of Letraset sheets containing
-                                        Lorem Ipsum passages, and more recently
-                                        with desktop publishing software like
-                                        Aldus PageMaker including versions of
-                                        Lorem Ipsum.
-                                    </p>
-                                </div>
-                            </div>
-                        </div>
+                </>
+            ) : (
+                <>
+                    {" "}
+                    <div className="container-fluid" style={{width:"3rem", height:"3rem", marginTop:"200px"}}>
+                        <div
+                            className="spinner-border text-primary"
+                            role="status"
+                        >
+                            <span className="sr-only">Loading...</span>
+                        </div>{" "}
                     </div>
-
-                    <div className="container">
-                        <div className="row bg-white m-3 p-5 border shadow-lg">
-                            <div className="col-sm">
-                                <h4 className="text-left mb-4 font-weight-bold">
-                                    Social Media
-                                </h4>
-                                <div className="row d-flex w-25 justify-content-between ml-2">
-                                    <i
-                                        className="fa fa-2x fa-facebook-square"
-                                        aria-hidden="true"
-                                    ></i>
-
-                                    <i
-                                        className="fa fa-2x fa-linkedin-square"
-                                        aria-hidden="true"
-                                    ></i>
-
-                                    <i
-                                        className="fa fa-2x fa-instagram"
-                                        aria-hidden="true"
-                                    ></i>
-
-                                    <i
-                                        className="fa fa-2x fa-whatsapp"
-                                        aria-hidden="true"
-                                    ></i>
-
-                                    <i
-                                        className="fa fa-2x fa-youtube-play"
-                                        aria-hidden="true"
-                                    ></i>
-                                </div>
-                            </div>
-                        </div>
-                    </div>
-                </div>
-            </>
-        ) : (
-            <>
-                {" "}
-                <div className="container-fluid">
-                    <div className="spinner-border text-primary" role="status">
-                        <span className="sr-only">Loading...</span>
-                    </div>{" "}
-                </div>
-            </>
-        )}</>;
+                </>
+            )}
+        </>
+    );
 }
 
 export default Profile;
